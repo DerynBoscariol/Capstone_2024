340b1582ca7063e0b9d17885146ff799afbac5c4
Deryn Boscariol <derynb@rogers.com>
2024-09-25 10:35:39 -0400

    testing w3 css

--- StagePass/src/components/Header.jsx
+++ StagePass/src/components/Header.jsx
@@ -2,7 +2,15 @@ import SiteName from "./SiteName";
 
 function Header() {
     return (
-    <SiteName/>
+        <header id="header" className="w3-container w3-deep-purple">
+        <div id="nameTag">
+            <SiteName/>
+            <h3 id="tag-line">Think of a tagline</h3>
+        </div>
+        <div id="">
+            <a href="/myprofile">Profile</a>
+        </div>
+    </header>
         )
     }
     export default Header;
